9d3aeee57ffee4027bf079429fa36352abf50e3d
Alexandret Menigaul <alexandre.menigault@gmail.com>
2024-07-04 14:52:57 +0000

    yes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api/', function(req, res) {
+app.get('/api', function(req, res) {
   const date = new Date()
   return res.json({
     unix: date.getTime(),
